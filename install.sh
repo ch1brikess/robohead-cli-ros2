a57909c0b77ea3a6047ec7783bfb3070431990c7
Kirill <kirmur2012@gmail.com>
2026-04-23 20:11:42 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
-sudo mkdir /opt/robohead-cli
-sudo touch /opt/robohead-cli/robohead
+sudo mkdir -p /opt/robohead-cli
+sudo mkdir -p ~/.config/robohead
 sudo cp $SCRIPT_DIR/source/robohead /opt/robohead-cli/robohead
+sudo cp $SCRIPT_DIR/source/config.json ~/.config/robohead/config.json
 sudo chmod +x /opt/robohead-cli/robohead
 
-sudo ln -s /opt/robohead-cli/robohead /usr/local/bin/robohead
+sudo ln -sf /opt/robohead-cli/robohead /usr/local/bin/robohead
